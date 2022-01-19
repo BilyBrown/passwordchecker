3e15136356cf2ce50658c70126e09819b1815165
BilyBrown <wdbrown03@gmail.com>
2022-01-18 20:59:24 -0500

    added comments

--- checkmypass.py
+++ checkmypass.py
@@ -2,6 +2,7 @@ import requests
 import hashlib
 import sys
 
+# the function that talks to the api and returns the goodies
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
@@ -9,6 +10,7 @@ def request_api_data(query_char):
         raise RuntimeError(f"Error Fetching: {res.status_code}, check the api and try again.")
     return res
 
+# the function that iterates through the list of hashes and grabs the count for the password/hash we are interested in
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
@@ -16,12 +18,15 @@ def get_password_leaks_count(hashes, hash_to_check):
             return count
     return 0
 
+# creating hashes and separating the hash so that we don't send our password over the internet
+# get a response from the api and grab how many times it occurs via the get_password_leaks_count function
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
     return get_password_leaks_count(response, tail)
 
+# primary function that ties it all together
 def main(args):
     for password in args:
         count = pwned_api_check(password)
